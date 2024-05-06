fbb8c6b30080bc4783810cbdf4e36738951cc3b4
ryosuke-sato <satouryousuke@satouryousukenoMacBook-Pro.local>
2024-05-06 16:47:39 +0900

    shadcn ui

---
+++ src/app/about/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+export default function About() {
+  return (
+    <div>
+      <h1 className="text-2xl font-bold m-4 flex justify-center">About</h1>
+      <div className="mt-4 text-center">
+        <Button>ボタン</Button>
+      </div>
+    </div>
+  );
+}
