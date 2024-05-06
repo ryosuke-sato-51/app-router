4125124071e59f1b8403af5697a544d2e90d8b43
ryosuke-sato <satouryousuke@satouryousukenoMacBook-Pro.local>
2024-05-06 17:29:03 +0900

    add biome

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,12 +1,17 @@
 import { Button } from "@/components/ui/button";
 
 export default function About() {
-  return (
-    <div>
-      <h1 className="text-2xl font-bold m-4 flex justify-center">About</h1>
-      <div className="mt-4 text-center">
-        <Button>ボタン</Button>
-      </div>
-    </div>
-  );
+	return (
+		<div>
+			<h1 className="text-2xl font-bold m-4 flex justify-center">About</h1>
+			<div className="mt-4 text-center">
+				<Button type="button">ボタン</Button>
+			</div>
+			<div className="mt-4 text-center">
+				<Button>
+					テストあああああテストあああああテストあああああテストあああああテストあああああ
+				</Button>
+			</div>
+		</div>
+	);
 }
